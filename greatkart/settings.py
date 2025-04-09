fda8345e8711983d83585531eb5e4f2285672f1b
kenan1525 <allattinmehmet@gmail.com>
2025-04-09 17:57:37 +0300

    sonmu

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -167,7 +167,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'tr-tr'
+LANGUAGE_CODE = 'en-en'
 
 TIME_ZONE = 'Europe/Istanbul'
 
